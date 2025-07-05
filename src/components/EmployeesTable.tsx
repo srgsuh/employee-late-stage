b24b200ece520128329fe79a9b043b7d4befce09
srgsuh <srg.suhoverhov@gmail.com>
2025-07-05 20:37:06 +0300

    Implemented timeout wrapper for delete employee. It is visible that AlertDialog awaits for a record to be deleted

--- src/components/EmployeesTable.tsx
+++ src/components/EmployeesTable.tsx
@@ -45,7 +45,15 @@ const EmployeesTable:FC<Props> = ({emplManager}: Props) => {
     throw error;
   }
 
-  const mutationDel = useEmployeesMutation((id:unknown)=>emplManager.deleteEmployee(id as string));
+  const longTimeDel = (id:unknown)=>{
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(emplManager.deleteEmployee(id as string));
+      }, 2000);
+    });
+  }
+
+  const mutationDel = useEmployeesMutation(longTimeDel);
   const mutationUpdate = useEmployeesMutation((updater:unknown) => emplManager.updateEmployee(updater as Updater));
 
   const page = useEmployeesPagination(s => s.page);
